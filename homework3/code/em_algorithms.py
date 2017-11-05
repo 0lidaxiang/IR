cd71b4d7a6733d16f8cfa18e61d10beba71dfaf4
0lidaxiang <lidaxiangregister@126.com>
2017-11-05 18:15:00 +0800

    modify some print result format.

--- homework3/code/em_algorithms.py
+++ homework3/code/em_algorithms.py
@@ -41,12 +41,12 @@ def e_step():
                 i = int(now_doc_WordIndex[ni][1])
                 sum_w_t_d = 0
                 for kk in range(topicNum):
-                    sum_w_t_d += math.log10( P_w_T[i][kk]) + math.log10(P_T_d[kk][d])
+                    sum_w_t_d +=  P_w_T[i][kk] * P_T_d[kk][d]
                 if sum_w_t_d <= 0:
                     sum_w_t_d = 1e-6
 
-                P_w_T_i_k = math.log10(P_w_T[i][k])
-                P_T_d_k_d = math.log10( P_T_d[k][d])
+                P_w_T_i_k = P_w_T[i][k]
+                P_T_d_k_d =  P_T_d[k][d]
                 index = index = d * (k+1)
                 P_T_wd[index][i] = (P_w_T_i_k * P_T_d_k_d) / sum_w_t_d
 def m_step():
@@ -67,10 +67,10 @@ def m_step():
                 d = dict_DocCountWord[i][j][0]
 
                 index = d * (k+1)
-                para1 = math.log10(int(dict_DocCountWord[i][j][1]))
-                para2 = math.log10(P_T_wd[index][i])
+                para1 = int(dict_DocCountWord[i][j][1])
+                para2 = P_T_wd[index][i]
                 # molecular
-                molecular += para1 + para2
+                molecular += para1 * para2
 
             if w_T_denominator <= 0:
                 w_T_denominator = 1e-6
@@ -88,10 +88,10 @@ def m_step():
             for dd in range(len(now_doc_WordIndex)):
                 _i = int(now_doc_WordIndex[dd][1])
                 index = d * (k+1)
-                mole_para1 = math.log10(int(count_w_d[ d ][dd][1]))
-                mole_para2 = math.log10(P_T_wd[index][_i])
+                mole_para1 = int(count_w_d[ d ][dd][1])
+                mole_para2 = P_T_wd[index][_i]
 
-                molecular += mole_para1 + mole_para2
+                molecular += mole_para1 * mole_para2
 
                 denominator += mole_para1
 
@@ -99,39 +99,35 @@ def m_step():
                 denominator = 1e-6
             P_T_d[k][d] = molecular / denominator
 
-max_likehood = 0
-def compute_max_likehood():
-    max_likehood = 0
+log_likelihood = 0
+def compute_log_likelihood():
+    log_likelihood = 0
 
     for i in range(wNumber):
         for j in range(len(dict_DocCountWord[i])):
             P_wT_Td = 0
             for k in range(topicNum):
                 P_wT_Td += P_w_T[i][k] * P_T_d[k][dict_DocCountWord[i][j][0]]
-            max_likehood += int(dict_DocCountWord[i][j][1]) * math.log10(P_d[j] * P_wT_Td)
-    return max_likehood
+            log_likelihood += int(dict_DocCountWord[i][j][1]) * math.log10(P_d[j] * P_wT_Td)
+    return log_likelihood
 
 def train():
     startTrain = datetime.now()
-    fname = "../maxLikehood_" + str(datetime.now().strftime('%Y-%m-%d')) + ".log"
+    fname = "../log_likelihood_" + str(datetime.now().strftime('%Y-%m-%d')) + ".log"
     f = open(fname, 'w')
-    for i in range(1, 20):
+    for i in range(1, 11):
         startE = datetime.now()
+
         e_step()
-        print("iteration %d, E-step excution time = %.10s"%(i,  datetime.now()-startE))
-
-        startM = datetime.now()
         m_step()
-        print("iteration %d, M-step excution time = %.10s"%(i,  datetime.now()-startM))
+        log_likelihood = compute_log_likelihood()
 
-        max_likehood = compute_max_likehood()
-
-        nowTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S');
-        f.write("%s  iteration %d, max-likelihood = %.10f\n"%(nowTime, i, max_likehood))
-        print("%s  iteration %d, max-likelihood = %.6f"%(nowTime, i, max_likehood))
+        nowTime = datetime.now()
+        f.write('{:<4d} {:20s}   log_likelihood = {:9f}\n'.format(i, nowTime.strftime('%Y-%m-%d %H:%M:%S'), log_likelihood))
+        print('{:<4d} excuteTime: {:9s}(sec) ,log_likelihood = {:9f}'.format(i, str(nowTime - startE).split(':', 3)[2],  log_likelihood))
     f.close()
-    print(str(i) + " train excution time is ", datetime.now()-startTrain)
 
-print("\ntrain start")
+
+print("\ntrain start -------------------- ")
 train()
-print("train stop\n")
+print("\ntrain excution time is ", datetime.now()-startTrain)
